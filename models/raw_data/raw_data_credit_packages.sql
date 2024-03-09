4b6597ada0a8b3016b401ebd2302889e9efc7631
e.kavani@yahoo.com <e.kavani@yahoo.com>
2024-03-09 20:49:52 +0000

    I have added some transformation model

--- models/raw_data/raw_data_credit_packages.sql
+++ models/raw_data/raw_data_credit_packages.sql
@@ -1,17 +1,17 @@
+-- models/raw_data/raw_data_requests.sql
+
+-- Configuring the model
+{{ config(
+    materialized='table',
+    unique_key='id_credit_package',
+    sort='signature_date'
+) }}
 
 
 -- Defining the raw_data_credit_packages model
 WITH raw_data_credit_packages AS (
     SELECT
-    id_credit_package,
-    signature_date,
-    start_date,
-    end_date,
-    credits_amount,
-    total_value_eur,
-    payment_cycle,
-    id_subscription,
-    id_organization
+    *
 FROM
     Qualifyze.raw_data_credit_packages
 )
---
+++ models/transformed_data/customer_engagement_evolution.sql
@@ -0,0 +1,29 @@
+
+-- Configuring the model
+{{ config(
+    materialized='table'
+) }}
+
+
+-- SQL query for Customer Engagement Evolution with Model Switch
+WITH CustomerEvolutionCTE AS (
+    SELECT
+        r.id_organization,
+        r.customer_name,
+        MAX(CASE WHEN r.audit_type = 'EXISTING_AUDIT' THEN 1 ELSE 0 END) AS has_existing_audit,
+        MAX(CASE WHEN r.audit_type = 'NEW_AUDIT' THEN 1 ELSE 0 END) AS has_new_audit,
+        MAX(CASE WHEN c.id_organization IS NOT NULL THEN 1 ELSE 0 END) AS has_subscription,
+        MAX(CASE WHEN r.created_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) AS has_activity_last_year
+    FROM
+        {{ ref('raw_data_request') }}  r
+    LEFT JOIN
+       {{ ref('raw_data_credit_packages') }}  c ON r.id_organization = c.id_organization
+    GROUP BY
+        r.id_organization, r.customer_name
+)
+
+SELECT
+    * FROM 
+    CustomerEvolutionCTE
+
+
