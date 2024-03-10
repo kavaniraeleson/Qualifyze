2c114a30e0a565aa762f21be234d9c1f7ea42915
e.kavani@yahoo.com <e.kavani@yahoo.com>
2024-03-10 00:35:44 +0000

    modified the customer_engagementt_model

--- models/transformed_data/customer_engagement_evolution.sql
+++ models/transformed_data/customer_engagement_evolution.sql
@@ -6,18 +6,18 @@
 
 
 -- SQL query for Customer Engagement Evolution with Model Switch
+--- 
 WITH Customer_evolution AS (
     SELECT
         r.id_organization,
         r.customer_name,
-        MAX(CASE WHEN r.audit_type = 'EXISTING_AUDIT' THEN 1 ELSE 0 END) AS has_existing_audit,
-        MAX(CASE WHEN r.audit_type = 'NEW_AUDIT' THEN 1 ELSE 0 END) AS has_new_audit,
-        MAX(CASE WHEN c.id_organization IS NOT NULL THEN 1 ELSE 0 END) AS has_subscription,
-        MAX(CASE WHEN r.created_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) AS has_activity_last_year
+        MAX(CASE WHEN r.created_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) AS has_activity_last_year,
+        MAX(CASE WHEN r.id_request IS NOT NULL THEN 'True' ELSE 'False' END) AS has_request,
+        MAX(CASE WHEN c.id_credit_package IS NOT NULL THEN 'True' ELSE 'False' END) AS has_credit_package
     FROM
         {{ ref('raw_data_request') }}  r
     LEFT JOIN
-       {{ ref('raw_data_credit_packages') }}  c ON r.id_organization = c.id_organization
+        {{ ref('raw_data_credit_packages') }}  c ON r.id_organization = c.id_organization
     GROUP BY
         r.id_organization, r.customer_name
 )
@@ -26,10 +26,12 @@ SELECT
     id_organization,
     customer_name,
     CASE
-        WHEN has_activity_last_year = 1 AND has_existing_audit = 1 AND has_new_audit = 1 THEN 'Transitioned'
-        WHEN has_activity_last_year = 1 AND has_existing_audit = 1 THEN 'Transactional Only'
-        WHEN has_activity_last_year = 1 AND has_new_audit = 1 THEN 'Subscription Only'
-        ELSE 'No Audit Data or Activity Last Year'
+        WHEN has_activity_last_year = 1 AND has_request = 'True' AND has_credit_package = 'True' THEN 'Transitioned'
+        WHEN has_activity_last_year = 1 AND has_request = 'True' THEN 'Transactional Only'
+        WHEN has_activity_last_year = 1 AND has_credit_package = 'True' THEN 'Subscription Only'
+        WHEN has_request = 'True' AND has_credit_package = 'False' THEN 'Transactional Only (No Credit Package)'
+        WHEN has_request = 'False' AND has_credit_package = 'True' THEN 'Subscription Only (No Request)'
+        ELSE 'No Transaction and Subscription'
     END AS customer_evolution
 FROM
     Customer_evolution
